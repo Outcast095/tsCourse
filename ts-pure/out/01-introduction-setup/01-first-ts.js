1094e18082e7210fd92ac0d0216195493192b77a
Karasaev Magomed <rostovmen95@gmail.com>
2026-01-22 19:52:22 +0300

    add ts config

---
+++ ts-pure/out/01-introduction-setup/01-first-ts.js
@@ -0,0 +1,13 @@
+"use strict";
+// npm install -g typescript   //установка typecript глобально 
+// npm uninstall -g typescript // удаление typecript глобально
+//npm install -D typescript //установка typecript локально 
+//npx tsc --init  // установка tsconfig.json
+// npm init // сгенерировать проект 
+// tsc -v  проверка версии typescript
+// tsc и название файла   запуск файла typescript    // tsc 01-first-ts.ts
+// node и название файла что бы запустить скомпилированный файл //node 01-first-ts.js
+{
+    const message = "Hello, world!";
+    console.log(message);
+}
